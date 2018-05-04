6f1f78522a97366af641d609c545eed92832caf7
Will Mitchell <wvmitchell@gmail.com>
2018-05-04 10:49:21 -0600

    MOAR commits

--- logger.js
+++ logger.js
@@ -3,3 +3,5 @@ console.log('Mod 3 is rad!!');
 console.log('War Eagle!');
 console.log('My first log!')
 console.log('Again again!')
+console.log('Rebasing is just THE best!')
+console.log('Semicolons are dumb')
